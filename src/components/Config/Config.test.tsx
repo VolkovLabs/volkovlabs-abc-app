217cc42d6c28f0eaa2a8ab05b983e2a754ed440f
Mikhail Volkov <47795110+mikhail-vl@users.noreply.github.com>
2022-05-01 23:01:16 -0400

    Remove Enable/Disable Config buttons, update README (#8)
    * Remove Enable/Disable Config buttons, update README
    
    * Fix tests
    
    * Rebuild using 8.5.1
    
    Co-authored-by: Mikhail <47795110+mikhailredis@users.noreply.github.com>

--- src/components/Config/Config.test.tsx
+++ src/components/Config/Config.test.tsx
@@ -18,91 +18,9 @@ const getPlugin = (overridePlugin: any = { meta: {} }) => ({
  */
 describe('Config', () => {
   /*
-   Initialization
-   */
-  describe('Initialization', () => {
-    it('If plugin is not enabled and meta is not set, state should have isEnabled = false', () => {
-      const plugin = getPlugin({});
-      const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
-      expect(wrapper.state().isEnabled).toBeTruthy();
-    });
-
-    it('If plugin is not enabled, state should have isEnabled = false', () => {
-      const plugin = getPlugin({ meta: { enabled: false } });
-      const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
-      expect(wrapper.state().isEnabled).toBeFalsy();
-    });
-
-    it('If plugin is enabled, state should have isEnabled = true', () => {
-      const plugin = getPlugin({ meta: { enabled: true } });
-      const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
-      expect(wrapper.state().isEnabled).toBeTruthy();
-    });
-  });
-
-  /*
-   Rendering
-   */
-  describe('rendering', () => {
-    it('If plugin is not configured, should show enable button', () => {
-      const plugin = getPlugin({ meta: { enabled: false } });
-      const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
-      const enableButton = wrapper.findWhere((node) => node.name() === 'Button' && node.text() === 'Enable');
-      expect(enableButton.exists()).toBeTruthy();
-    });
-
-    it('If plugin is configured, should show disable buttons', () => {
-      const plugin = getPlugin({ meta: { enabled: true } });
-      const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
-      const disableButton = wrapper.findWhere((node) => node.name() === 'Button' && node.text() === 'Disable');
-      expect(disableButton.exists()).toBeTruthy();
-    });
-
-    it('Enable button should call onEnable method', () => {
-      const plugin = getPlugin({ meta: { enabled: false } });
-      const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
-      const testedMethod = jest.spyOn(wrapper.instance(), 'onEnable').mockImplementation(() => null);
-      wrapper.instance().forceUpdate();
-      const enableButton = wrapper.findWhere((node) => node.name() === 'Button' && node.text() === 'Enable');
-      enableButton.simulate('click');
-      expect(testedMethod).toHaveBeenCalled();
-    });
-
-    it('Disable button should call onDisable method', () => {
-      const plugin = getPlugin({ meta: { enabled: true } });
-      const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
-      const testedMethod = jest.spyOn(wrapper.instance(), 'onDisable').mockImplementation(() => null);
-      wrapper.instance().forceUpdate();
-      const disableButton = wrapper.findWhere((node) => node.name() === 'Button' && node.text() === 'Disable');
-      disableButton.simulate('click');
-      expect(testedMethod).toHaveBeenCalled();
-    });
-  });
-
-  /*
    Methods
    */
   describe('Methods', () => {
-    it('onDisable should call updatePluginSettings method', () => {
-      const plugin = getPlugin({ meta: { enabled: true } });
-      const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
-      const testedMethod = jest
-        .spyOn(wrapper.instance(), 'updatePluginSettings')
-        .mockImplementation(() => Promise.resolve(null as any));
-      wrapper.instance().onDisable();
-      expect(testedMethod).toHaveBeenCalledWith({ enabled: false, jsonData: {}, pinned: false });
-    });
-
-    it('onEnable should call updatePluginSettings method', () => {
-      const plugin = getPlugin({ meta: { enabled: true } });
-      const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
-      const testedMethod = jest
-        .spyOn(wrapper.instance(), 'updatePluginSettings')
-        .mockImplementation(() => Promise.resolve(null as any));
-      wrapper.instance().onEnable();
-      expect(testedMethod).toHaveBeenCalledWith({ enabled: true, jsonData: {}, pinned: true });
-    });
-
     it('updatePluginSettings should make post request', () => {
       const plugin = getPlugin({ meta: { enabled: true, id: 'app' } });
       const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
