dd4a17ae056f4021316003a43a1859932626d2e1
Alex Simonok <sashasimonok@gmail.com>
2023-06-02 09:15:29 +0300

    Rewrite unit tests with testing-library/react

--- src/components/Config/Config.test.tsx
+++ src/components/Config/Config.test.tsx
@@ -1,6 +1,15 @@
-import { shallow } from 'enzyme';
 import React from 'react';
+import { getBackendSrv } from '@grafana/runtime';
+import { act, render, screen, fireEvent } from '@testing-library/react';
+import { TestIds } from '../../constants';
 import { Config } from './Config';
+
+/**
+ * Mock @grafana/runtime
+ */
+jest.mock('@grafana/runtime', () => ({
+  getBackendSrv: jest.fn(),
+}));
 
 /*
  Plugin
@@ -21,17 +30,29 @@ describe('Config', () => {
    Methods
    */
   describe('Methods', () => {
-    it('updatePluginSettings should make post request', () => {
+    it('updatePluginSettings should make post request', async () => {
       const plugin = getPlugin({ meta: { enabled: true, id: 'app' } });
-      const wrapper = shallow<Config>(<Config plugin={plugin} query={null as any} />);
       const postRequestMock = jest.fn();
-      wrapper.instance()['backendSrv'] = {
-        post: postRequestMock,
-      } as any;
 
-      const settings = { enabled: true, jsonData: {}, pinned: true };
-      wrapper.instance().updatePluginSettings(settings);
-      expect(postRequestMock).toHaveBeenCalledWith(`api/plugins/${plugin.meta.id}/settings`, settings);
+      jest.mocked(getBackendSrv).mockImplementationOnce(
+        () =>
+          ({
+            post: postRequestMock,
+          } as any)
+      );
+
+      render(<Config plugin={plugin} query={null as any} />);
+
+      expect(screen.getByTestId(TestIds.config.root)).toBeInTheDocument();
+      expect(screen.getByTestId(TestIds.config.buttonUpdate)).toBeInTheDocument();
+
+      await act(() => fireEvent.click(screen.getByTestId(TestIds.config.buttonUpdate)));
+
+      expect(postRequestMock).toHaveBeenCalledWith(`api/plugins/${plugin.meta.id}/settings`, {
+        enabled: true,
+        jsonData: {},
+        pinned: true,
+      });
     });
   });
 
---
+++ src/constants/tests.ts
@@ -0,0 +1,13 @@
+/**
+ * Tests Identifiers
+ */
+export const TestIds = {
+  config: {
+    root: 'data-testid config',
+    buttonUpdate: 'data-testid config button-update',
+  },
+  rootPage: {
+    content: 'data-testid root-page content',
+    loadingIndicator: 'data-testid root-page loading-indicator',
+  },
+};
