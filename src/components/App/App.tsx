f7adf73479ad45873512a1b63d725a4c99f76c26
Mikhail <mikhail@volkovlabs.io>
2023-10-14 16:37:01 -0400

    Updates

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { AppRootProps } from '@grafana/data';
 import { AppSettings } from '../../types';
-import { Route, Switch } from 'react-router-dom';
 import { Home } from '../Home';
 
 /**
